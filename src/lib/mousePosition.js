c7bd4a81286dab3e31dc1f96d6d032fe4f2d3a9a
jycouet <jycouet@gmail.com>
2024-02-11 22:58:44 +0100

    fix?

--- src/lib/mousePosition.js
+++ src/lib/mousePosition.js
@@ -1,16 +1,18 @@
 import { readable } from "svelte/store";
 
 export default readable({ x: 0, y: 0 }, (set) => {
-  document.body.addEventListener("mousemove", move);
+  try {
+    document.body.addEventListener("mousemove", move);
 
-  function move(event) {
-    set({
-      x: event.clientX,
-      y: event.clientY,
-    });
-  }
+    function move(event) {
+      set({
+        x: event.clientX,
+        y: event.clientY,
+      });
+    }
 
-  return () => {
-    document.body.removeEventListener("mousemove", move);
-  };
+    return () => {
+      document.body.removeEventListener("mousemove", move);
+    };
+  } catch (error) {}
 });
